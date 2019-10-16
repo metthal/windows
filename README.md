9f675eaa9e2def4120984718756a9f62a3b93251
Marek Milkovič <marek.milkovic@avast.com>
2019-10-16 07:34:27 +0200

    Trying out to build VS2019

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ```
-docker build --build-arg CMAKE_VERSION=3.15.4 -t metthal/windows:vs2017-cmake3.15.4 -f Dockerfile.vs-cmake .
-docker build --build-arg PYTHON_VERSION=3.7.4 -t metthal/windows:vs2017-cmake3.15.4-python3.7.4 -f Dockerfile.vs-cmake-python .
-docker build --build-arg PYTHON_VERSION=3.8.0 -t metthal/windows:vs2017-cmake3.15.4-python3.8.0 -f Dockerfile.vs-cmake-python .
-docker build --build-arg PYTHON_VERSION=3.6.8 -t metthal/windows:vs2017-cmake3.15.4-python3.6.8 -f Dockerfile.vs-cmake-python .
+./build.sh <VS_VERSION> <CMAKE_VERSION> [<PYTHON_VERSION>]
+
+VS_VERSION - Either 2017 or 2019.
+CMAKE_VERSION - CMake version in format <MAJOR>.<MINOR>.<PATCH>
+PYTHON_VERSION - Python 3 version in format 3.<MINOR>.<PATCH>. Optional.
 ```
---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -eq 2 ]; then
+	DOCKERFILE=Dockerfile.vs-cmake
+elif [ $# -eq 3 ]; then
+	DOCKERFILE=Dockerfile.vs-cmake-python
+else
+	exit 1
+fi
+
+if [ "$1" == "2017" ]; then
+	VS_VERSION=15
+elif [ "$1" == "2019" ]; then
+	VS_VERSION=16
+else
+	exit 1
+fi
+
+CMAKE_VERSION="$2"
+PYTHON_VERSION="$3"
+
+IMAGE_NAME="metthal/vs$1-cmake${CMAKE_VERSION}"
+DOCKER_BUILD_ARGS=(--build-arg VS_VERSION=${VS_VERSION} --build-arg CMAKE_VERSION=${CMAKE_VERSION})
+if [ ! -z "$PYTHON_VERSION" ]; then
+	DOCKER_BUILD_ARGS[1]="VS_VERSION=$1"
+	DOCKER_BUILD_ARGS+=(--build-arg PYTHON_VERSION=${PYTHON_VERSION})
+	IMAGE_NAME="${IMAGE_NAME}-python${PYTHON_VERSION}"
+fi
+
+echo "Run this command:"
+echo
+echo "docker build ${DOCKER_BUILD_ARGS[@]} -t ${IMAGE_NAME} -f ${DOCKERFILE}"
